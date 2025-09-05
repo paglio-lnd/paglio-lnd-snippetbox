b78415a8089a4ffc1c1ea17eff3943cc3d5a7fb0
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2025-09-05 20:03:47 +0200

    chore: add dummy route to retrieve snippet by id

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func handleGetSnippetByID(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Here is your snippet with ID %d", id)
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("GET /snippets/{id}", handleGetSnippetByID)
 
 	log.Print("Starting server on :4000")
 
